0c0cf516f48cda858262299a0200b00149535c13
Shivangi <shivangit1111@gmail.com>
2023-03-16 15:09:54 +0530

    feat: impliment bundle

--- app/src/main/java/com/example/cafeapp/MainActivity.kt
+++ app/src/main/java/com/example/cafeapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cafeapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.cafeapp.databinding.ActivityMainBinding
@@ -12,7 +13,16 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+          binding.btn.setOnClickListener {
+              val bundle = Bundle()
+              bundle.putString("Veg",binding.editText1.text.toString())
+              bundle.putString("nonVeg",binding.editText2.text.toString())
+              bundle.putString("Sweet",binding.editText3.text.toString())
 
+              val intent = Intent(this,OrderActivity::class.java)
+              intent.putExtras(bundle)
+              startActivity(intent)
+          }
         setContentView(binding.root)
     }
 }
