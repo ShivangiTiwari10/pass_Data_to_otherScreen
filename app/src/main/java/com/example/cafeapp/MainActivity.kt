ecc3d93ff7c593946c7d7bb8bb97340d5a0361e1
Shivangi <shivangit1111@gmail.com>
2023-03-16 19:49:14 +0530

    refactor: add companion object

--- app/src/main/java/com/example/cafeapp/MainActivity.kt
+++ app/src/main/java/com/example/cafeapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.cafeapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -8,19 +9,22 @@ import com.example.cafeapp.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    companion object{
+        const val KEY ="com.example.cafeapp.MainActivity.KEY"
+    }
+    @SuppressLint("SuspiciousIndentation")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
 
           binding.btn.setOnClickListener {
-              val bundle = Bundle()
-              bundle.putString("Veg",binding.editText1.text.toString())
-              bundle.putString("nonVeg",binding.editText2.text.toString())
-              bundle.putString("Sweet",binding.editText3.text.toString())
+
+              val orderPlaced ="${binding.editText1.toString()+" "},${binding.editText2.toString()+" "},${binding.editText3}"
 
               val intent = Intent(this,OrderActivity::class.java)
-              intent.putExtras(bundle)
+              intent.putExtra(KEY,orderPlaced)
               startActivity(intent)
           }
         setContentView(binding.root)
