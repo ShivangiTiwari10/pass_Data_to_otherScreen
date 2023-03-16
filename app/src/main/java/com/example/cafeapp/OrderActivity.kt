e5a8d0bca822244dab8b4298563c7ab03074ea92
Shivangi <shivangit1111@gmail.com>
2023-03-16 16:01:24 +0530

    feat: get Data from mainActivity

--- app/src/main/java/com/example/cafeapp/OrderActivity.kt
+++ app/src/main/java/com/example/cafeapp/OrderActivity.kt
@@ -1,11 +1,24 @@
 package com.example.cafeapp
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.cafeapp.databinding.ActivityOrderBinding
 
 class OrderActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOrderBinding
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_order)
+        binding =ActivityOrderBinding.inflate(layoutInflater)
+
+        val bundle = intent.extras
+        if (bundle != null){
+            binding.textView3.text = "Weg = ${bundle.getString("Veg")}"
+            binding.textView4.text = "NonVeg = ${bundle.getString("nonVeg")}"
+            binding.text5.text = "Sweet = ${bundle.getString("Sweet")}"
+        }
+
+        setContentView(binding.root)
     }
 }
